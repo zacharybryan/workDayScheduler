1b35de49c96857f718bb8c5b2c71296c4376d7b4
Zach Bryan <zacharybryan@icloud.com>
2020-08-28 14:46:26 -0700

    formating of dates

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ console.log(now);
 $("#currentDay").text(now);
 
 $("#back").on("click", function() {
-    let backDate = moment().subtract(1, 'days');
+    let backDate = moment().subtract(1, 'days').format("dddd, MMMM D, YYYY");
     console.log(backDate);
     $("#currentDay").text(backDate);
 });
 
 $("#forward").on("click", function() {
-    let forwardDate = moment().add(1, 'days');
+    let forwardDate = moment().add(1, 'days').format("dddd, MMMM D, YYYY");
     console.log(forwardDate);
     $("#currentDay").text(forwardDate);
 });
