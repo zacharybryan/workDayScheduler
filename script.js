792d6fec123b47b34938b22ee823e4eb5add7b1e
Zach Bryan <zacharybryan@icloud.com>
2020-08-28 14:43:10 -0700

    addind date switcher

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var now = moment().format("dddd, MMMM D, YYYY");
 console.log(now);
 
 $("#currentDay").text(now);
+
+$("#back").on("click", function() {
+    let backDate = moment().subtract(1, 'days');
+    console.log(backDate);
+    $("#currentDay").text(backDate)
+});
+
+$("#forward").on("click", function() {
+    
+});
