466388094c34ca2c9669340a42b49a827e0c1b10
Zach Bryan <zacharybryan@icloud.com>
2020-09-03 18:21:18 -0700

    formatting script

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 'use strict'
 $(document).ready(function() {
     
-var currentDay = moment().format("dddd, MMMM D, YYYY");
+    var currentDay = moment().format("dddd, MMMM D, YYYY");
 
-console.log(currentDay);
+    console.log(currentDay);
 
-$("#currentDay").text(currentDay);
+    $("#currentDay").text(currentDay);
 
 // CLICK FUNCTIONS
 
 
-$("#back").on("click", function() {
+    $("#back").on("click", function() {
     let backDate = moment().subtract(1, 'days').format("dddd, MMMM D, YYYY");
     console.log(backDate);
     $("#currentDay").text(backDate);
-});
+    });
 
-$("#forward").on("click", function() {
+    $("#forward").on("click", function() {
     let forwardDate = moment().add(1, 'days').format("dddd, MMMM D, YYYY");
     console.log(forwardDate);
     $("#currentDay").text(forwardDate);
-});
+    });
 });
