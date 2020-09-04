5123ecc9f6df33cc853c70a309816bb486fbba41
Zach Bryan <zacharybryan@icloud.com>
2020-09-03 19:08:45 -0700

    added memory pull to screen from local storage

--- script.js
+++ script.js
@@ -31,4 +31,15 @@ $(document).ready(function() {
 
         localStorage.setItem(time, value);
     })
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
 });
